4741adc7bd44b09935ba38b72d67df0aa32aa882
Griffith88 <dobrvn@gmail.com>
2021-08-04 14:30:41 +0300

    version 1.5.1 Added rotation into logging, checking

--- server.py
+++ server.py
@@ -1,14 +1,27 @@
 import os
+from logging import DEBUG, Formatter,basicConfig
 
-from datetime import date
-from logging import basicConfig
+from logging.handlers import TimedRotatingFileHandler
 from werkzeug.exceptions import abort
 from cofigure_file import Phone
 from flask import Flask, Response, send_file, request
 
-log_path = os.path.normpath(os.path.join(os.path.dirname(__file__), 'logs', f'{date.today()}-log.log'))
-basicConfig(filename=log_path)
+log_path = os.path.normpath(os.path.join(os.path.dirname(__file__), 'logs', 'main.log'))
+timed_handler = TimedRotatingFileHandler(
+    filename=log_path,
+    when='midnight',
+    interval=1,
+    backupCount=10
+)
+
+timed_handler.setFormatter(Formatter(
+    '[%(asctime)s] %(levelname)s in %(module)s: %(message)s')
+)
+
+basicConfig(level=DEBUG)
 app = Flask('yealink')
+if not app.debug:
+    app.logger.addHandler(timed_handler)
 
 
 @app.route('/<mac>.cfg')
@@ -16,7 +29,7 @@ def configure_telephone(mac):
     if mac.startswith('y'):
         abort(404)
     user_agent = request.headers.get('User-Agent')
-    app.logger.info(f'Пришел запрос на автоконфигурацию от устройства {user_agent}')
+    app.logger.info(f'Запрос автоконфигурации: устройство {user_agent} c ip {request.remote_addr}')
     phone = Phone(mac, user_agent)
     try:
         config_file = phone.get_configuration_file_name()
@@ -24,7 +37,7 @@ def configure_telephone(mac):
         config_file = None
         app.logger.warning(f'{val.args}')
         abort(404)
-
+    app.logger.info(f'{user_agent} c ip {request.remote_addr} получил конфигурацию!')
     with open(config_file, 'r', encoding='utf8') as f:
         return Response(f.read(), mimetype='text/plain')
 
@@ -36,4 +49,4 @@ def update_firmware(firmware_name):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=5000, debug=False, threaded=True)
